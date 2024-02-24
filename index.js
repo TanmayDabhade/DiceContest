b5fbfa38fb9535918875efe2a1793d428cce8127
TanmayDabhade <107470247+TanmayDabhade@users.noreply.github.com>
2024-02-23 21:35:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ function reloadPage() {
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //Generates a number form 1-6 for player 1
 
-var randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //creates the image address for the number generated for player 1
+var randomImageSource1 = "dice" + randomNumber1 + ".png"; //creates the image address for the number generated for player 1
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1); //assigns the image address to the respective image tag
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1; //Generates a number form 1-6 for player 2
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //creates the image address for the number generated for player 2
+var randomImageSource2 = "dice" + randomNumber2 + ".png"; //creates the image address for the number generated for player 2
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); //assigns the image address to the respective image tag
 
 
 
-// Conditionals determine winner/draw.
+// Conditionals determine the winner/draw.
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h2").innerHTML = "🚩 Play 1 Wins!";
 }
